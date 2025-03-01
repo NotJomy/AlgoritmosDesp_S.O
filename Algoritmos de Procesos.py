650748c0382f71490af28834c5fede251f16265e
Samuel Herrera Marín <samuel.herrera@utp.edu.co>
2025-03-01 14:57:53 -0500

    Creada clase APP y Grafica

--- Algoritmos de Procesos.py
+++ Algoritmos de Procesos.py
@@ -1,98 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import tkinter as tk
+import pygame
+from ttkthemes import ThemedTk
 
-class Procesos:
-    def __init__(self, nombre, tiempo_llegada, rafaga):
-        self.nombre = nombre 
-        self.tiempo_llegada = tiempo_llegada
-        self.rafaga = rafaga
-        self.tiempo_final = 0
-        self.tiempo_inicio = 0
-        self.prioridad = 0
+        
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Algoritmos de Procesos")
+        self.root.geometry("800x600")
+        self.root.resizable(False, False)
+        self.root.config(bg="black")
 
-    def set_prioridad(self):
-        self.prioridad = input(f"Que prioridad quiere tener el proceso {self.nombre}: ") #Definir priridad por teclado
+        self.label = tk.Label(self.root, text="Algoritmos de Procesos", font=("Arial", 20), bg="black", fg="white")
+        self.label.pack(pady=20)
 
-    def crear_proceso(self):
-        self.nombre = input("Ingrese el nombre del proceso: ")
-        self.tiempo_llegada = int(input("Ingrese el tiempo de llegada del proceso: "))
-        self.rafaga = int(input("Ingrese la rafaga del proceso: "))
+        self.button_iniciar = tk.Button(self.root, text="Iniciar", font=("Arial", 15), bg="black", fg="white", command=self.abrir_grafica)
+        self.button_iniciar.pack(pady=20)
 
-def crear_procesos():
-    procesos = [] #Lista que me permitira almacenar los procesos
-    cantidad_procesos = int(input("Cuantos procesos desea crear: ")) #Defino la cantidad de procesos que se crearan
-    for i in range(cantidad_procesos): #Recorro la cantidad de procesos que se crearan
-        proceso = Procesos("", 0, 0) #Creo un objeto de la clase Procesos
-        proceso.crear_proceso() #Llamo al metodo crear_proceso
-        procesos.append(proceso) #Agrego el proceso a la lista
-    return procesos
+        self.button_salir = tk.Button(self.root, text="Salir", font=("Arial", 15), bg="black", fg="white", command=self.root.quit)
+        self.button_salir.pack(pady=20)
 
-def Fifo(procesos): #Funcion que implementa el algoritmo FIFO
+    def abrir_grafica(self):
+        self.root.withdraw()  # Oculta la ventana principal
+        
+        # Crea una nueva instancia de la clase 'Grafica'
+        self.grafica_window = Grafica(self.root)
 
-    fifo_list = [] #Lista que me permitira almacenar los procesos
 
-    for proceso in procesos: #Recorro la lista de procesos
-        fifo_list.append(proceso) #Agrego el proceso a la lista
+class Grafica:
+    def __init__(self, root):
+        self.root = tk.Toplevel(root)  # Crea una nueva ventana de tipo Toplevel, es decir, una ventana secundaria
+        self.root.title("Graficar")
+        self.root.geometry("800x600")
+        self.root.resizable(False, False)
+        self.root.config(bg="black")
 
-    tiempo_ejecucion = 0 #Me permite conocer el tiempo de ejecucion en le que me encuentro
-    tiempo_espera = 0 #Me permite conocer el tiempo de espera de todos los procesos
-    tiempo_sistema = 0 #Me permite conocer el tiempo de sistema de todos los procesos
-    total = 0 #Me permite conocer el tiempo total de ejecucion de todos los procesos
+        self.label = tk.Label(self.root, text="Ingrese la cantidad de procesos que desea ejecutar (máximo 10)", font=("Arial", 20), bg="black", fg="white")
+        self.label.pack(padx=10, pady=10)
 
-    for proceso in fifo_list:
-        total += proceso.rafaga #Calculo el tiempo total de ejecucion de todos los procesos  
+        self.num_procesos = tk.Entry(self.root, font=("Arial", 20))
+        self.num_procesos.pack(padx=10, pady=10)
 
-    while (total > 0): #Mientras el tiempo total sea mayor a 0
-        for proceso in fifo_list:
-            if(tiempo_ejecucion >= proceso.tiempo_llegada): #Verifico si el proceso ya existe 
-                    
-                tiempo_espera += tiempo_ejecucion - proceso.tiempo_llegada #Calculo el tiempo de espera
-                tiempo_ejecucion += proceso.rafaga #Calculo el tiempo de ejecucion
+        self.button_ejecutar = tk.Button(self.root, text="Ejecutar Algoritmo", font=("Arial", 15), bg="black", fg="white", command=self.ejecutar_algoritmo)
+        self.button_ejecutar.pack(pady=20)
 
-                #Estas me permiteran graficar el diagrama de Gantt
-                proceso.tiempo_final = proceso.rafaga + tiempo_ejecucion #Calculo el tiempo final del proceso
-                proceso.tiempo_inicio = tiempo_ejecucion - proceso.rafaga #Calculo el tiempo de inicio del proceso
+        self.button_volver = tk.Button(self.root, text="Volver al inicio", font=("Arial", 15), bg="black", fg="white", command=self.volver_inicio)
+        self.button_volver.pack(pady=20)
 
-                tiempo_sistema += tiempo_ejecucion - proceso.tiempo_llegada #Calculo el tiempo de sistema
-                    
-                total -= proceso.rafaga #Resto el tiempo de ejecucion del proceso al tiempo total
-                    
-                fifo_list.remove(proceso) #Elimino el proceso de la lista
-                break
+    def ejecutar_algoritmo(self):
+        try:
+            num_procesos = int(self.num_procesos.get())  # Obtener el número de procesos
+            if num_procesos <= 10:
+                self.mostrar_animacion(num_procesos)  # Ejecutar animación
+            else:
+                print("Por favor, ingrese un número de procesos válido (máximo 10).")
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
 
-    print("El tiempo de espera es :", tiempo_espera/4) #Imprimo el tiempo de espera
-    print("El tiempo de sistema es: ", tiempo_sistema/4) #Imprimo el tiempo de sistema
+    def mostrar_animacion(self, num_procesos):
+        # Aquí puedes agregar el código para la animación de los algoritmos
+        print(f"Ejecutando animación con {num_procesos} procesos.")  # Ejemplo de mensaje
 
-def SJF(procesos): #Funcion que implementa el algoritmo SJF
+        # Puedes utilizar matplotlib o cualquier otro medio para la animación aquí
+        plt.figure()
+        plt.plot(np.random.rand(10))
+        plt.title(f"Animación con {num_procesos} procesos")
+        plt.show()
 
-    sjf_list = [] #Lista que me permitira almacenar los procesos
+    def volver_inicio(self):
+        self.root.destroy()  # Cierra la ventana de 'Grafica'
+        self.root.master.deiconify()  # Muestra la ventana principal de nuevo
+    
 
-    for proceso in procesos: #Recorro la lista de procesos
-        sjf_list.append(proceso) #Agrego el proceso a la lista
-
-    sjf_list.sort(key=lambda x: x.rafaga) #Ordeno la lista de procesos por rafaga
-
-    Fifo(sjf_list) #Llamo a la funcion FIFO    
-
-def Prioridad(procesos): #Funcion que implementa el algoritmo de prioridad
-
-    for proceso in procesos: #Recorro la lista de procesos
-        proceso.set_prioridad() #Defino la prioridad de cada proceso
-
-    prioridad_list = [] #Lista que me permitira almacenar los procesos
-
-    for proceso in procesos: #Recorro la lista de procesos
-        prioridad_list.append(proceso) #Agrego el proceso a la lista
-
-    prioridad_list.sort(key=lambda x: x.prioridad) #Ordeno la lista de procesos por rafaga
-
-    Fifo(prioridad_list) #Llamo a la funcion
-
-procesos = crear_procesos()
-
-print("-----------------------------")
-Fifo(procesos)
-print("-----------------------------")
-SJF(procesos)
-print("-----------------------------")
-Prioridad(procesos)
+if __name__ =="__main__":
+    root = ThemedTk(theme="arc")
+    app = App(root)
+    root.mainloop()
---
+++ Procesos.py
@@ -0,0 +1,85 @@
+class Procesos:
+    def __init__(self, nombre, tiempo_llegada, rafaga):
+        self.nombre = nombre 
+        self.tiempo_llegada = tiempo_llegada
+        self.rafaga = rafaga
+        self.tiempo_final = 0
+        self.tiempo_inicio = 0
+        self.prioridad = 0
+
+    def set_prioridad(self):
+        self.prioridad = input(f"Que prioridad quiere tener el proceso {self.nombre}: ") #Definir priridad por teclado
+
+    def crear_proceso(self):
+        self.nombre = input("Ingrese el nombre del proceso: ")
+        self.tiempo_llegada = int(input("Ingrese el tiempo de llegada del proceso: "))
+        self.rafaga = int(input("Ingrese la rafaga del proceso: "))
+
+def crear_procesos():
+    procesos = [] #Lista que me permitira almacenar los procesos
+    cantidad_procesos = int(input("Cuantos procesos desea crear: ")) #Defino la cantidad de procesos que se crearan
+    for i in range(cantidad_procesos): #Recorro la cantidad de procesos que se crearan
+        proceso = Procesos("", 0, 0) #Creo un objeto de la clase Procesos
+        proceso.crear_proceso() #Llamo al metodo crear_proceso
+        procesos.append(proceso) #Agrego el proceso a la lista
+    return procesos
+
+def Fifo(procesos): #Funcion que implementa el algoritmo FIFO
+
+    fifo_list = [] #Lista que me permitira almacenar los procesos
+
+    for proceso in procesos: #Recorro la lista de procesos
+        fifo_list.append(proceso) #Agrego el proceso a la lista
+
+    tiempo_ejecucion = 0 #Me permite conocer el tiempo de ejecucion en le que me encuentro
+    tiempo_espera = 0 #Me permite conocer el tiempo de espera de todos los procesos
+    tiempo_sistema = 0 #Me permite conocer el tiempo de sistema de todos los procesos
+    total = 0 #Me permite conocer el tiempo total de ejecucion de todos los procesos
+
+    for proceso in fifo_list:
+        total += proceso.rafaga #Calculo el tiempo total de ejecucion de todos los procesos  
+
+    while (total > 0): #Mientras el tiempo total sea mayor a 0
+        for proceso in fifo_list:
+            if(tiempo_ejecucion >= proceso.tiempo_llegada): #Verifico si el proceso ya existe 
+                    
+                tiempo_espera += tiempo_ejecucion - proceso.tiempo_llegada #Calculo el tiempo de espera
+                tiempo_ejecucion += proceso.rafaga #Calculo el tiempo de ejecucion
+
+                #Estas me permiteran graficar el diagrama de Gantt
+                proceso.tiempo_final = proceso.rafaga + tiempo_ejecucion #Calculo el tiempo final del proceso
+                proceso.tiempo_inicio = tiempo_ejecucion - proceso.rafaga #Calculo el tiempo de inicio del proceso
+
+                tiempo_sistema += tiempo_ejecucion - proceso.tiempo_llegada #Calculo el tiempo de sistema
+                    
+                total -= proceso.rafaga #Resto el tiempo de ejecucion del proceso al tiempo total
+                    
+                fifo_list.remove(proceso) #Elimino el proceso de la lista
+                break
+
+    print("El tiempo de espera es :", tiempo_espera/4) #Imprimo el tiempo de espera
+    print("El tiempo de sistema es: ", tiempo_sistema/4) #Imprimo el tiempo de sistema
+
+def SJF(procesos): #Funcion que implementa el algoritmo SJF
+
+    sjf_list = [] #Lista que me permitira almacenar los procesos
+
+    for proceso in procesos: #Recorro la lista de procesos
+        sjf_list.append(proceso) #Agrego el proceso a la lista
+
+    sjf_list.sort(key=lambda x: x.rafaga) #Ordeno la lista de procesos por rafaga
+
+    Fifo(sjf_list) #Llamo a la funcion FIFO    
+
+def Prioridad(procesos): #Funcion que implementa el algoritmo de prioridad
+    for proceso in procesos: #Recorro la lista de procesos
+        proceso.set_prioridad() #Defino la prioridad de cada proceso
+
+    prioridad_list = [] #Lista que me permitira almacenar los procesos
+
+    for proceso in procesos: #Recorro la lista de procesos
+        prioridad_list.append(proceso) #Agrego el proceso a la lista
+
+    prioridad_list.sort(key=lambda x: x.prioridad) #Ordeno la lista de procesos por rafaga
+
+    Fifo(prioridad_list) #Llamo a la funcion
